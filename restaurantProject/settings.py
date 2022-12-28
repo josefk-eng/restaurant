5c9a6e75f8abb50d58669979e6232a66fa2dc04a
joseph <kigozijosefed1993@gmail.com>
2022-12-28 14:43:24 +0300

    media url

--- restaurantProject/settings.py
+++ restaurantProject/settings.py
@@ -127,3 +127,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
